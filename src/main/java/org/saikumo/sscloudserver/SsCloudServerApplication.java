f4caf558662e9a04ac3bd3320f18b31d99b07c02
Xufuhao <saikumo11@gmail.com>
2022-09-20 14:08:43 +0800

    [Create] initial commit

---
+++ src/main/java/org/saikumo/sscloudserver/SsCloudServerApplication.java
@@ -0,0 +1,13 @@
+package org.saikumo.sscloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsCloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsCloudServerApplication.class, args);
+	}
+
+}
